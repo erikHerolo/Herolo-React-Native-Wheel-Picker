f63dd5e4f666b87f6ab31f8afcd57655876605b1
Yonatan Lozinsky <yonatan.lozinsky@herolo.co.il>
2019-07-06 16:26:48 +0300

    refactor(styles): move styles from component file

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useRef, useEffect } from "react";
-import styled from "styled-components/native";
 
 import { Text } from "react-native";
-
+import { ScrollWrapper, Item, ItemText, Sv, Cover } from './styles/wheel';
 import { scrollToNumber } from './utils/functions';
 
 const createList = (options, itemHeight, itemStyles, textStyles, spaces) => {
@@ -81,41 +80,5 @@ const App = ({
   );
 };
 
-// TODO: Remove margin in production
-const ScrollWrapper = styled.View`
-  overflow: hidden;
-  height: ${props => props.height}px;
-  width: ${props => props.width}px;
-  justify-content: space-between;
-  align-items: center;
-  margin: 50px;
-`;
-
-const Item = styled.View`
-  height: ${props => props.itemHeight}px;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ItemText = styled.Text`
-  color: black;
-`;
-
-const Sv = styled.ScrollView`
-  width: 100%;
-`;
-
-const Cover = styled.View`
-  position: absolute;
-  top: ${({ itemHeight, items }) => itemHeight * (items / 2 - 0.5)}px;
-  left: -25%;
-  height: ${props => props.itemHeight}px;
-  width: 150%;
-  border-top-width: ${props => props.borderWidth}px;
-  border-top-color: ${props => props.borderColor};
-  background-color: rgba(255, 255, 255, 0);
-  z-index: 1;
-`;
 
 export default App;
---
+++ styles/wheel.js
@@ -0,0 +1,38 @@
+import styled from "styled-components/native";
+
+// TODO: Remove margin in production
+export const ScrollWrapper = styled.View`
+  overflow: hidden;
+  height: ${props => props.height}px;
+  width: ${props => props.width}px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px;
+`;
+
+export const Item = styled.View`
+  height: ${props => props.itemHeight}px;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ItemText = styled.Text`
+  color: black;
+`;
+
+export const Sv = styled.ScrollView`
+  width: 100%;
+`;
+
+export const Cover = styled.View`
+  position: absolute;
+  top: ${({ itemHeight, items }) => itemHeight * (items / 2 - 0.5)}px;
+  left: -25%;
+  height: ${props => props.itemHeight}px;
+  width: 150%;
+  border-top-width: ${props => props.borderWidth}px;
+  border-top-color: ${props => props.borderColor};
+  background-color: rgba(255, 255, 255, 0);
+  z-index: 1;
+`;
